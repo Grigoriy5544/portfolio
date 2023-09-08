0e26cd9fbcfdaafd045e9a33f08ac25cbf49440b
Grigoriy5544 <gregkotsenko@yandex.ru>
2023-09-08 17:37:27 +0400

    fix bag

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,8 @@
+const pg = require('pg')
 const { Sequelize } = require('sequelize')
-const sequelize = new Sequelize(process.env.DB_URL + '?sslmode=require')
+
+const sequelize = new Sequelize(process.env.DB_URL + '?sslmode=require', {
+    dialectModule: pg
+})
 
 module.exports = sequelize
